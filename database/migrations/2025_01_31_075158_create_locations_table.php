83b7fa8867951c384f9b81f340dc70c295c5bc0c
Mahdi Bagheri <mb.programmer@yahoo.com>
2025-02-14 23:50:18 +0330

    fix(DB): add use statement

--- database/migrations/2025_01_31_075158_create_locations_table.php
+++ database/migrations/2025_01_31_075158_create_locations_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use \Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
 
@@ -31,8 +32,8 @@ return new class extends Migration {
         });
 
         // Add spatial columns
-        \Illuminate\Support\Facades\DB::statement("ALTER TABLE $this->tableName ADD COLUMN center POINT NULL after is_capital");
-        \Illuminate\Support\Facades\DB::statement("ALTER TABLE $this->tableName ADD COLUMN area MULTIPOLYGON NULL after center");
+      DB::statement("ALTER TABLE $this->tableName ADD COLUMN center POINT NULL after is_capital");
+      DB::statement("ALTER TABLE $this->tableName ADD COLUMN area MULTIPOLYGON NULL after center");
     }
 
     /**
