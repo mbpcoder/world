4370b770f8b8d0aac33b800df4e3b32797558b56
Mahdi Bagheri <mb.programmer@yahoo.com>
2025-02-14 18:01:34 +0330

    fix(Facade): add some doc block

--- src/Facades/World.php
+++ src/Facades/World.php
@@ -3,7 +3,19 @@
 namespace TheCoder\World\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use TheCoder\World\Repositories\CityRepository;
+use TheCoder\World\Repositories\ContinentRepository;
+use TheCoder\World\Repositories\CountryRepository;
+use TheCoder\World\Repositories\ProvinceRepository;
 
+/**
+ * @method static ContinentRepository continents()
+ * @method static CountryRepository countries()
+ * @method static ProvinceRepository provinces()
+ * @method static CityRepository cities()
+ *
+ * @see \TheCoder\World\World
+ */
 class World extends Facade
 {
     protected static function getFacadeAccessor()
