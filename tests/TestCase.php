25f503ca3717170874779b6612b11c7e6f9eb523
Mahdi Bagheri <mb.reckless@gmail.com>
2025-02-15 18:57:58 +0330

    fix(migration): run migrate and seed once

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,20 +2,24 @@
 
 namespace TheCoder\World\Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Orchestra\Testbench\TestCase as Orchestra;
 use TheCoder\World\WorldServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
-    use RefreshDatabase;
+    protected static bool $migrated = false;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->artisan('migrate')->run();
-        $this->artisan('world:seed')->run();
+        if (!static::$migrated) {
+            $this->artisan('migrate')->run();
+            $this->artisan('world:seed')->run();
+            $this->artisan('cache:clear');
+            static::$migrated = true;
+        }
     }
 
     protected function getPackageProviders($app)
